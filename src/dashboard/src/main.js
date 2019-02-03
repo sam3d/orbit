fa06ff7fe20969bcbf8dfa307cadcca71e926247
Sam Holmes <samholmes1337@gmail.com>
2019-02-03 17:29:59 +0000

    Complete loader implementation

--- src/dashboard/src/main.js
+++ src/dashboard/src/main.js
@@ -3,10 +3,17 @@ import App from "./App";
 import router from "./router";
 import store from "./store";
 
+import "reset-css";
+import "@/styles/main.scss";
+
 const vue = new Vue({
   store,
   router,
   render: h => h(App)
 });
 
-vue.$mount("#dashboard");
+setTimeout(() => {
+  // Emulate loading time
+  vue.$mount("#dashboard");
+  window.removeLoader();
+}, 1000);
