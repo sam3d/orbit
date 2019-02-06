e095486f0ca8c3729f87be55571f9352d96078a1
Sam Holmes <samholmes1337@gmail.com>
2019-02-06 17:20:42 +0000

    Begin console API interaction

--- src/console/src/api/index.js
+++ src/console/src/api/index.js
@@ -1,8 +1,26 @@
 import axios from "axios";
 
-const api = axios.create({
+const instance = axios.create({
   baseURL: process.env.VUE_APP_ORBIT_API_URL,
-  validateStatus: status => true
+  validateStatus: status => true, // Always succeed
+  redirect: true // Custom redirect handler option
 });
 
-export default api;
+/**
+ * Custom redirect handler. If certain requests fail with authentication errors,
+ * we want to be able to clear out the local user store and then redirect to the
+ * login page with an error.
+ */
+instance.interceptors.response.use(
+  res => {
+    if (!res.config.redirect) return res;
+    return res;
+  },
+
+  err => {
+    console.log(err.message);
+    return {};
+  }
+);
+
+export default instance;
--- src/console/src/store/index.js
+++ src/console/src/store/index.js
@@ -7,14 +7,13 @@ import api from "@/api";
 const store = new Vuex.Store({
   actions: {
     /**
-     * The "init" user action is responsible for setting up the entire state of
-     * the store. That means that if the "user" route from the Orbit API server
-     * tells us that there are no user and the system requires a setup, then
-     * there can't be any user information and we simply redirect to that.
+     * The init action is responsible for retrieving the overall cluster state
+     * and then (in the majority of cases) populating it with the current user
+     * information. During the setup phase, the user route won't get called as
+     * there can't be any user information.
      */
     async init() {
-      const res = await api.get("/user");
-      console.log(res);
+      const res = await api.get("/state", { redirect: false });
     }
   }
 });
