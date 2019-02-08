9bd53f8ad06b3f76361eb5d8734f9c49aeeb2cf0
Sam Holmes <samholmes1337@gmail.com>
2019-02-08 13:00:43 +0000

    Change how default options are handled

--- src/console/vue.config.js
+++ src/console/vue.config.js
@@ -2,11 +2,15 @@
  * Bind the environment variables so that vue cli can interpolate them.
  * https://cli.vuejs.org/guide/mode-and-env.html#using-env-variables-in-client-side-code
  */
-["ORBIT_API_URL"].forEach(name => {
-  const env = process.env[name];
-  if (!env) throw `Missing environment variable: ${name}`;
-  process.env["VUE_APP_" + name] = env;
-});
+const defaultOptions = {
+  ORBIT_API_URL: "/api"
+};
+
+for (let name in defaultOptions) {
+  const env = process.env[name] || defaultOptions[name];
+  const key = "VUE_APP_" + name;
+  process.env[key] = env;
+}
 
 module.exports = {
   devServer: { port: 3000 }
