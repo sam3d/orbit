1b7d687d3e8c8483f3c97992696dada0056808f8
Sam Holmes <samholmes1337@gmail.com>
2019-02-18 19:59:44 +0000

    Prepare API directory

---
+++ src/engine/api/api.go
@@ -0,0 +1,5 @@
+package api
+
+// Server is the root instance for the API server.
+type Server struct {
+}
---
+++ src/engine/api/doc.go
@@ -0,0 +1,4 @@
+// Package api provides an HTTP interface to the engine operations that exposes
+// both on a port and on a UNIX socket. The CLI will, by default, dial into the
+// UNIX socket that is exposed and consume the HTTP API.
+package api
