ba2fc5c0120f3e7c7b2f3d15a1f0f9d7a27d225b
Sam Holmes <samholmes1337@gmail.com>
2019-05-01 12:16:29 +0100

    Successfully generate LetsEncrypt data and convert to PEM

--- src/engine/store_certificates.go
+++ src/engine/store_certificates.go
@@ -5,7 +5,10 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/x509"
+	"crypto/x509/pkix"
 	"encoding/hex"
+	"encoding/pem"
 	"fmt"
 	"log"
 
@@ -48,8 +51,17 @@ func (s *Store) RenewCertificates() error {
 		return errors.Wrap(err, "could not create ACME client")
 	}
 
-	// Keep track of all of the ACME challenges for all certificates.
-	var acmeChallenges []*acme.Challenge
+	// Request is the overarching structure to keep track of the certificates
+	// during the renewal process.
+	type Request struct {
+		Certificate    *Certificate
+		Challenges     []*acme.Challenge
+		Authorizations []*acme.Authorization
+		Errors         []error
+	}
+
+	// requests is for keeping track of the certificate requests.
+	var requests []*Request
 
 	// Retrieve the challenges for the certificates.
 	for _, cert := range s.state.Certificates {
@@ -59,6 +71,10 @@ func (s *Store) RenewCertificates() error {
 		if !cert.AutoRenew {
 			continue
 		}
+
+		// Prepare the overall request object for this certificate.
+		req := &Request{Certificate: &cert}
+		requests = append(requests, req)
 
 		// Keep track of a list of challenges for the domains in this certificate.
 		var challenges []Challenge
@@ -84,7 +100,7 @@ func (s *Store) RenewCertificates() error {
 			}
 
 			// Keep track of the acme challenge so that they can be accepted.
-			acmeChallenges = append(acmeChallenges, challenge)
+			req.Challenges = append(req.Challenges, challenge)
 
 			// Retrieve the challenge properties.
 			path := client.HTTP01ChallengePath(challenge.Token)
@@ -122,27 +138,95 @@ func (s *Store) RenewCertificates() error {
 		return errors.Wrap(err, "could not restart the edge routers")
 	}
 
-	// Now that the update is complete, we want to alert that we are ready to
-	// accept all of the challenges that we have been issued.
-	for _, c := range acmeChallenges {
-		if _, err := client.Accept(context.Background(), c); err != nil {
-			return errors.Wrap(err, "could not confirm acceptance of challenge")
+	// Now that the load balancers have been updated, we can perform all of the
+	// operations required to actually requisition the certificates.
+
+	// Accept all of the challenges.
+	for _, r := range requests {
+		for _, c := range r.Challenges {
+			if _, err := client.Accept(context.Background(), c); err != nil {
+				return errors.Wrap(err, "could not confirm acceptance of challenge")
+			}
 		}
 	}
 
-	// Next step, we need to wait for the authorizations from the challenges.
-	for _, c := range acmeChallenges {
-		auth, err := client.WaitAuthorization(context.Background(), c.URI)
-		if err != nil {
-			// If this doesn't work, we don't want to fail as there are other
-			// challenges that we need to run. Instead, let's just log the error with
-			// the respective URL and continue on.
-			log.Printf("[ERR] certs: Could not authorize certificate (%s)", c.URI)
+	// Authorize all of the challenges.
+	for _, r := range requests {
+		for _, c := range r.Challenges {
+			auth, err := client.WaitAuthorization(context.Background(), c.URI)
+			if err != nil {
+				// If an authorization fails, we can't make the certificate request. All
+				// of the authorizations need to succeed in a certificate to be able to
+				// undertake certificate retrieval.
+				log.Printf("[ERR] certs: Could not authorize certificate (%s)", c.URI)
+				r.Errors = append(r.Errors, err)
+				continue
+			}
+
+			// The authorization succeeded, keep track of it.
+			r.Authorizations = append(r.Authorizations, auth)
+		}
+	}
+
+	// For each of requests with successful certificates, construct the
+	// certificate signing requests and issue them.
+	for _, r := range requests {
+		if len(r.Errors) > 0 {
+			log.Printf("[ERR] certs: Skipping certificate %s, as %d of the domains encountered errors", r.Certificate.ID, len(r.Errors))
 			continue
 		}
 
-		fmt.Println("\n\nWE ARE AUTHORIZED!!!")
-		fmt.Printf("%+v\n\n", auth)
+		// Generate a private key for this certificate.
+		certKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+		if err != nil {
+			return errors.Wrap(err, "could not generate private key")
+		}
+
+		// Construct the certificate request.
+		req := &x509.CertificateRequest{
+			Subject:  pkix.Name{CommonName: r.Certificate.Domains[0]},
+			DNSNames: r.Certificate.Domains,
+		}
+
+		// Create the actual signing request.
+		csr, err := x509.CreateCertificateRequest(rand.Reader, req, certKey)
+		if err != nil {
+			return errors.Wrap(err, "could not create certificate request")
+		}
+
+		// Create the encoded certificate with the signing request.
+		der, url, err := client.CreateCert(context.Background(), csr, 0, true)
+		if err != nil {
+			return errors.Wrap(err, "could not create certificate")
+		}
+		fmt.Println("URL:", url)
+
+		// A full chain certificate is simply multiple certificates appended
+		// together. Important to note is that you don't just append the bytes, but
+		// you literally have a single file with multiple "--BEGIN CERTIFICATE--"
+		// and "--END CERTIFICATE--" blocks just one after the other. That's what
+		// this next bit does.
+		var cert []byte
+		for _, b := range der {
+			block := pem.EncodeToMemory(&pem.Block{
+				Type:  "CERTIFICATE",
+				Bytes: b,
+			})
+			cert = append(cert, block...)
+		}
+
+		// Now we need to convert the private key to PEM format.
+		derCertKey, err := x509.MarshalECPrivateKey(certKey)
+		if err != nil {
+			return errors.Wrap(err, "could not marshal private key to DER")
+		}
+		pemCertKey := pem.EncodeToMemory(&pem.Block{
+			Type:  "PRIVATE KEY",
+			Bytes: derCertKey,
+		})
+
+		// Update the certificate with the new LetsEncrypt certificate data.
+
 	}
 
 	return nil
