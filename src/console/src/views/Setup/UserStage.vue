b257e2fd81e489cd8b56d3220c4748b9db20dd65
Sam Holmes <samholmes1337@gmail.com>
2019-05-02 16:14:55 +0100

    Add sanitised user fields

--- src/console/src/views/Setup/UserStage.vue
+++ src/console/src/views/Setup/UserStage.vue
@@ -6,5 +6,78 @@
       can always change this later. This provides complete access to your entire
       cluster, so please ensure it is as secure as possible.
     </p>
+
+    <label>Username</label>
+    <input
+      v-model="user.username"
+      type="text"
+      name="username"
+      placeholder="admin"
+    />
+
+    <label>Email address</label>
+    <input
+      v-model="user.email"
+      type="email"
+      name="email"
+      :placeholder="placeholderEmail"
+    />
+
+    <label>Password</label>
+    <input type="password" name="password" placeholder="Password" />
+
+    <Button text="Continue" />
   </div>
 </template>
+
+<script>
+import Button from "@/components/Button";
+
+export default {
+  components: { Button },
+
+  data() {
+    return {
+      user: {
+        username: "",
+        email: "",
+        password: ""
+      }
+    };
+  },
+
+  computed: {
+    // Generate a placeholder email based on the domain name and the user name
+    // specified.
+    placeholderEmail() {
+      const { username } = this.user;
+      const [domain] = document.location.host.split(":");
+      return `${username ? username : "admin"}@${domain}`;
+    }
+  },
+
+  watch: {
+    // Sanitize the user fields on input.
+    user: {
+      deep: true,
+      handler() {
+        const { username, email } = this.user;
+
+        this.user.username = username
+          .toLowerCase()
+          .split(" ")
+          .join("");
+
+        this.user.email = email
+          .toLowerCase()
+          .split(" ")
+          .join("");
+      }
+    }
+  },
+
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped></style>
