1cb7a208afb80039188ab23e24fbd28504aade42
Sam Holmes <samholmes1337@gmail.com>
2019-05-02 15:26:46 +0100

    Create user account configuration stub

---
+++ src/console/src/views/Setup/UserStage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h2>Admin user</h2>
+    <p class="subheader">
+      Create the administrator user account that you will use to sign in. You
+      can always change this later. This provides complete access to your entire
+      cluster, so please ensure it is as secure as possible.
+    </p>
+  </div>
+</template>
