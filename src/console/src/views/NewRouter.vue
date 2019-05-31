751cd2fcf00c68791dddac01f25d16e17607b9be
Sam Holmes <samholmes1337@gmail.com>
2019-05-31 13:06:24 +0100

    Add diagrams and more content

--- src/console/src/views/NewRouter.vue
+++ src/console/src/views/NewRouter.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>
-    <h1>Create a new router</h1>
+  <LoadingSlider v-if="loading" />
+  <div class="sidebar-screen" v-else>
+    <h1>Add a new router</h1>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      loading: true
+    };
+  },
+
+  mounted() {
+    this.load();
+  },
+
+  methods: {
+    async load() {}
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
